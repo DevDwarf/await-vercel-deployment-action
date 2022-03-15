de75adb6fca17b8710622bb6018617e94018f46a
Armand <armandjvanrensburg@gmail.com>
2022-03-15 07:47:37 +0200

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+import {
+  ConfigInterface
+} from "./types"
+
+/**
+ * The configuration options for the action
+ */
+const config: ConfigInterface = {
+  VERCEL_BASE_ENDPOINT: ""
+}
+
+export default config
---
+++ src/types/ConfigInterface.ts
@@ -0,0 +1,9 @@
+/**
+ * Defines the configuration types
+ */
+export interface ConfigInterface {
+  /**
+   * Base url to connect to the Vercel API
+   */
+  VERCEL_BASE_ENDPOINT: string
+}
